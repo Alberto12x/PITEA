4bf86a515d62f3e7b55cb5d86fb55cb26919bc2c
Alberto12x <alberm33@ucm.es>
2025-07-30 14:00:47 +0200

    feat(procesamiento_qsl/estructura_cualquiera) regex para OCR

--- pitea/pitea/procesamiento_qsl/procesamiento_qsl.py
+++ pitea/pitea/procesamiento_qsl/procesamiento_qsl.py
@@ -48,6 +48,7 @@ class Procesamiento_datos_qsl:
 
         self.__dic_campos["TIME"] = hora_formateada
 
+   
         
     def __estructura_cualquiera(self, input_text):
         """
@@ -56,33 +57,80 @@ class Procesamiento_datos_qsl:
         que los datos no están separados por espacios ni nada, entendiendo todo como un único string.
         Además tenemos en cuenta los fallos típicos de OCR, como el cambio de 0 por O, o 1 por I, etc.
         """
+        equivalencias = {
+            '0': '[0o]',
+            '1': '[1il]',
+            '2': '[2s]',
+            '5': '[5z]',
+            '8': '[8b]',
+            'a': '[a]',
+            'b': '[b8]',
+            'c': '[c]',
+            'd': '[d]',
+            'e': '[e3]',
+            'g': '[g9]',
+            'i': '[i1l]',
+            'l': '[l1i]',
+            'o': '[o0]',
+            'q': '[q]',
+            's': '[s2]',
+            't': '[t7]',
+            'z': '[z5]',
+        }
+
+        def modo_a_regex(modo: str) -> str:
+            resultado = ''
+            for c in modo:
+                key = c.lower()
+                if key in equivalencias:
+                    resultado += equivalencias[key]
+                else:
+                    resultado += f"[{c.lower()}]"
+            return resultado
+        
+        def grupo_modos_a_regex(grupo: str) -> str:
+            modos = grupo.split('|')
+            return '|'.join(modo_a_regex(modo) for modo in modos)
+
+        grupo = "January|February|March|April|May|June|July|August|September|October|November|December"
+        meses_tolerante = grupo_modos_a_regex(grupo)
 
         # Modos SSTV, por ejemplo
-        patron_modo = r"(" + "|".join(constantes.MODOS_VALIDOS_SSTV) + r")"
+        patron_modo = r"(" + "|".join(modo_a_regex(modo) for modo in constantes.MODOS_VALIDOS_SSTV) + r")"
 
-        patron_qra = r"([A-R]{2}\d{2}(?:[a-x]{2})?|[A-Z]{1,2}\d{1}[A-Z]{1,3})"
+
+        patron_qra = r"([A-R125890]{2}[0-9ISZO]{2}(?:[a-x1250]{2})?|[A-Z125890]{1,2}[0-9ISZO][A-Z125890]{1,3})"
 
         patrones_date = [
-            r"((?:0?[1-9]|[12][0-9]|3[01])[-\/\.](?:0?[1-9]|1[012])[-\/\.](?:\d{2}|\d{4}))",  # DD/MM/YYYY o D/M/YY
-            r"(\d{4}[-\/\.](?:0?[1-9]|1[012])[-\/\.](?:0?[1-9]|[12][0-9]|3[01]))",  # YYYY-MM-DD (ISO)
-            r"((?:January|February|March|April|May|June|July|August|September|October|November|December)\s?(?:0?[1-9]|[12][0-9]|3[01]),?\s?\d{4})",  # Mes Nombre DD, YYYY
-            r"((?:0?[1-9]|[12][0-9]|3[01])\s?(?:January|February|March|April|May|June|July|August|September|October|November|December)\s??\d{4})",  # DD Mes Nombre YYYY
+            r"((?:[0O]?[1-9ISZ]|[12ISZ][0-9ISZO]|3[01OI])[-\/\.](?:[O0]?[1-9ISZ]|1[012ISZ])[-\/\.](?:[0-9ISZO]{2}|[0-9ISZO]{4}))",  # DD/MM/YYYY o D/M/YY
+            r"([0-9ISZO]{4}[-\/\.](?:[O0]?[1-9ISZ]|1[012OISZ])[-\/\.](?:[O0]?[1-9ISZ]|[12ISZ][0-9ISZO]|3[01OI]))",  # YYYY-MM-DD (ISO)
+            rf"((?:{meses_tolerante})\s?(?:[O0]?[1-9ISZ]|[12ISZ][0-9ISZO]|3[01OI])[,/]?\s?[0-9ISZO]{{4}})",  # Mes Nombre DD, YYYY
+            rf"((?:[O0]?[1-9ISZ]|[12ISZ][1-9ISZO]|3[01OI])\s?(?:{meses_tolerante}\s??[0-9ISZO]{4}))",  # DD Mes Nombre YYYY
         ]
 
         patrones_hora = [
-            r"((?:[01]?\d|2[0-3])[:\.][0-5]\d(?:[:\.][0-5]\d)?)",  # 24h con minutos y opcional segundos
-            r"((?:0?[1-9]|1[0-2])[:\.][0-5]\d\s?(?:AM|PM|am|pm))",  # 12h con minutos + AM/PM
-            r"((?:[01]\d|2[0-3])[0-5]\d)",  # 24h sin separador, HHMM
-            r"((?:0?[1-9]|1[0-2])\s??(?:AM|PM|am|pm))",  # 12h sin minutos, solo HH AM/PM
-            r"((?:[01]?\d|2[0-3])[:\.][0-5]\d(?:[:\.][0-5]\d)?\s?(?:UTC|GMT|Z|z)(?:[+\-]\d{2}(?::?\d{2})?)?)",  # 24h con zona horaria
-            r"((?:0?[1-9]|1[0-2])[:\.][0-5]\d\s?(?:AM|PM|am|pm)\s?(?:UTC|GMT|Z|z)(?:[+\-]\d{2}(?::?\d{2})?)?)",  # 12h con AM/PM y zona horaria
+            r"((?:[01IO]?[0-9ISZO]|2[0-3])[:\.][0-5][0-9ISZO](?:[:\.][0-5][0-9ISZO])?)",  # 24h con minutos y opcional segundos
+            r"((?:[O0]?[1-9ISZ]|1[0-2])[:\.][0-5][0-9ISZO]\s?(?:AM|PM))",  # 12h con minutos + AM/PM
+            r"((?:[01IO][0-9ISZO]|2[0-3])[0-5][0-9ISZO])",  # 24h sin separador, HHMM
+            r"((?:[O0]?[1-9ISZ]|1[0-2])\s??(?:AM|PM))",  # 12h sin minutos, solo HH AM/PM
+            r"((?:[01IO]?[0-9ISZO]|2[0-3])[:\.][0-5][0-9ISZO](?:[:\.][0-5][0-9ISZO])?\s?(?:UTC|GMT|Z|z)(?:[+\-][0-9ISZO]{2}(?::?[0-9ISZO]{2})?)?)",  # 24h con zona horaria
+            r"((?:[O0]?[1-9ISZ]|1[0-2])[:\.][0-5][0-9ISZO]\s?(?:AM|PM)\s?(?:UTC|GMT|Z|z)(?:[+\-][0-9ISZO]{2}(?::?[0-9ISZO]{2})?)?)",  # 12h con AM/PM y zona horaria
         ]
 
-        patron_freq = r"(\d{1,3}(?:\.\d{1,3})?\s?(?:kHz|KHz|KHZ|MHz|MHZ|hz|Hz)?)"
+        patron_freq = r"([0-9ISZO]{1,3}(?:\.[0-9ISZO]{1,3})?\s?(?:kH[Z2]|KH[Z2]|KH[Z2]|MH[Z2]|MH[Z2]|h[Z2]|H[Z2])?)"
 
-        patron_qth = r"[A-R]{2}[0-9]{2}(?:[A-X]{2})?"  # QTH sin ^ y $
+        patron_qth = r"[A-R125890]{2}[1-9ISZO]{2}(?:[A-X125890]{2})?"  
 
-        patron_rst = r"((?:RST|RS)?[ :\-]?\s?[1-5][1-9]{1,2})"
+        todas_posibilidades_rst = (
+            constantes.MODOS_SNR |
+            constantes.MODOS_RST_COMPLETO |
+            constantes.MODOS_RS
+        )
+        patron_rst = r"((?:R[5S]T|R[5S])?[ :\-]?\s?[1-5ISZ][1-9ISZ]{1,2})"
+
+        patron_rst = r"(" + "|".join(modo_a_regex(modo) for modo in todas_posibilidades_rst) + r")"
+
+         
 
         self.__dic_campos = {}
 
